e16f20a9c45214efeec195e1961cef8734b30218
Alex1s <Alex1s@users.noreply.github.com>
2026-02-21 18:13:24 +0100

    update copy_patch.sh

--- copy_patch.sh
+++ copy_patch.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+set -e
+set -x
 
-docker cp esphome/esphome/const.py esphome:/esphome/esphome/const.py
-docker cp esphome/esphome/components/output/float_output.cpp esphome:/esphome/esphome/components/output/float_output.cpp
-docker cp esphome/esphome/components/output/float_output.h esphome:/esphome/esphome/components/output/float_output.h
-docker cp esphome/esphome/components/output/__init__.py esphome:/esphome/esphome/components/output/__init__.py
+DOCKER_IMPL="sudo podman"
+$DOCKER_IMPL cp esphome/esphome/const.py esphome:/esphome/esphome/const.py
+$DOCKER_IMPL cp esphome/esphome/components/output/float_output.cpp esphome:/esphome/esphome/components/output/float_output.cpp
+$DOCKER_IMPL cp esphome/esphome/components/output/float_output.h esphome:/esphome/esphome/components/output/float_output.h
+$DOCKER_IMPL cp esphome/esphome/components/output/__init__.py esphome:/esphome/esphome/components/output/__init__.py
